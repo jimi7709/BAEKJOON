4b1a6bd204d97648a751a220904cdcd3cce1e9cc
Jimi <qkrwoals1588@gmail.com>
2022-01-11 16:18:50 +0900

    1297

---
+++ 1297/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+//26분
+using namespace std;
+
+int main(void) {
+
+	int D;//D는 대각선의 길이
+	int H;// H는 높이의 비
+	int W;// W는 너비의 비
+
+	cin >> D >> H >> W;
+
+	if (D >= 5 && D <= 1000 && H >= 1 && H <= 99 && W >= 2 && W <= 100 && H < W) {
+
+
+
+
+	}
+
+	return 0;
+}
